5492188fa2c758f8e003d43c8ec468991ff015f0
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-03-15 22:13:51 -0300

    spa

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({
+		fallback: '200.html'
+	}) },
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
 			filename.includes('node_modules') ? undefined : { runes: true }
