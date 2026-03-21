584b272f420f030db0eda849748d425d9b7f4d7a
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-03-21 14:30:23 -0300

    v 0.0.0.2

--- build/_app/immutable/entry/start.64JH2Kip.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/-4r3prnh.js";import{A as t}from"../chunks/D9tOaJrg.js";export{t as load_css,r as start};
---
+++ build/_app/immutable/entry/start.DXSz9bgn.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Bx88KUya.js";import{A as t}from"../chunks/DqQWdaM6.js";export{t as load_css,r as start};
