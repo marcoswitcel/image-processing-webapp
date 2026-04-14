ade9dad94da2eeed093cecaa280e6aa3c522a55b
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-04-13 22:01:59 -0300

    WIP: adicionando nós

--- src/lib/components/NodeEditor.svelte
+++ src/lib/components/NodeEditor.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
 	import { EditableFilterNode } from '$lib/filter-graph/index.svelte';
 
+	interface Props {
+        width?: number
+		height?: number
+		nodes: EditableFilterNode[]
+    }
+
+    const { width = 0, height = 0, nodes = $bindable() }: Props  = $props();
+	const svgOffsetX = $state(0);
+	const svgOffsetY = $state(0);
+
+	const viewBox = $derived(`${svgOffsetX} ${svgOffsetY} ${width} ${height}`);
+
 	const lineWidth = 3;
 	const borderRadius = 6;
 
-	const nodes: EditableFilterNode[] = $state([
-		new EditableFilterNode(),
-		new EditableFilterNode()
-	]);
-	nodes[1].x = 300;
-
-	nodes[0].out.push(nodes[1]);
-	nodes[1].in.push(nodes[0]);
+	// @todo joão, remover esse bloco quando permitir selecionar um bloco e depois o outro para estabelecer a conexão
+	{
+		nodes[1].x = 300;
+	
+		nodes[0].out.push(nodes[1]);
+		nodes[1].in.push(nodes[0]);
+	}
 
 	const lines = $derived(
 		nodes.flatMap(node => node.out.map(out => [node, out] as  [EditableFilterNode, EditableFilterNode]))
@@ -48,7 +59,8 @@
 </script>
 
 <svelte:window onmousemove={handleMouseMove} onmouseup={handleMouseUp}></svelte:window>
-<svg viewBox="0 0 600 600">
+
+<svg viewBox={viewBox}>
 
     {#each lines as line (line[0].id + line[1].id )}
         <line x1={line[0].x} y1={line[0].y} x2={line[1].x} y2={line[1].y} stroke="black" stroke-width={lineWidth} />
@@ -63,7 +75,6 @@
 
 <style>
 	svg {
-		width: 600px;
 		border: 1px dotted black;
 		user-select: none;
 	}
--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,18 +1,51 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import Button from '$lib/components/Button.svelte';
 	import NodeEditor from '$lib/components/NodeEditor.svelte';
+	import { EditableFilterNode } from '$lib/filter-graph/index.svelte';
+	import { innerHeight, innerWidth } from 'svelte/reactivity/window';
+
+	const nodes: EditableFilterNode[] = $state([
+		new EditableFilterNode(),
+		new EditableFilterNode(),
+	]);
+	
+	function add() {
+		nodes.push(new EditableFilterNode());
+	}
 </script>
 
-
 <div class="page">
-	<h1>Editor</h1>
-	<p>Pendente implementação</p>
-	<NodeEditor></NodeEditor>
-	<a href={resolve('/')} title="Câmera">Câmera</a>
+	<h1 class="title">Editor</h1>
+	<a href={resolve('/')} title="Câmera" class="link">Voltar para a câmera</a>
+
+	<NodeEditor width={innerWidth.current} height={innerHeight.current} nodes={nodes} ></NodeEditor>
+	<Button label="Adicionar Filtro" onclick={() => add()}></Button>
 </div>
 
 <style>
 	.page {
-		padding: 1em;
+		padding: 0;
+	}
+	.title {
+		position: fixed;
+		font-size: 2em;
+		margin: 0;
+		padding: 0;
+		top: 1em;
+		left: 1em;
+		user-select: none;
+		pointer-events: none;
+	}
+	.link {
+		position: fixed;
+		left: 1em;
+		bottom: 1em;
+		user-select: none;
+	}
+	:global(.button) {
+		position: fixed;
+		right: 1em;
+		bottom: 1em;
 	}
 </style>
