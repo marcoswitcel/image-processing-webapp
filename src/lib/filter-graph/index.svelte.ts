0e47ac76ea212b5c2621e0154560dddabd7deaba
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-04-15 21:42:57 -0300

    .

--- src/lib/filter-graph/index.svelte.ts
+++ src/lib/filter-graph/index.svelte.ts
@@ -25,6 +25,9 @@ export class EditableFilterNode {
 	y = $state(0);
 
 	public connect(to: EditableFilterNode): void {
+		// Por hora um filtro só pode apontar para outro uma única vez e o filtro alvo só pode ser apontado uma vez
+		if (this.out.includes(to) || to.in.includes(this)) return;
+
 		this.out.push(to);
 		to.in.push(this);
 	}
